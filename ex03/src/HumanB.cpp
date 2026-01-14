2863c6a7406251acac56e2e2a69a9c16886f5539
gpollast <gpollast@student.42.fr>
2026-01-14 16:32:36 +0100

    (fix): cpp01 really finished

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 17:56:19 by gpollast          #+#    #+#             */
-/*   Updated: 2026/01/09 20:34:45 by gpollast         ###   ########.fr       */
+/*   Updated: 2026/01/14 16:10:39 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ HumanB::HumanB(const std::string& name) : _weapon(NULL) ,_name(name) {
 
 void HumanB::attack() const
 {
-	std::cout << this->_name << " attacks with their " << _weapon->getType() << '\n';
+	if (this->_weapon)
+		std::cout << this->_name << " attacks with their " << _weapon->getType() << '\n';
+	else
+		std::cout << this->_name << " cannot attack (rip)" << '\n';
 }
 
 void	HumanB::setWeapon(const Weapon& weapon) {
